a98114170ec666263548785bf0926f4c838cf8e2
Kyiewi <kyrie.norriega@gmail.com>
2025-03-31 18:32:12 +0800

    program that removes the spaces at the end without using rstrip()

---
+++ remove_end_spaces_without_rstrip().py
@@ -0,0 +1,12 @@
+# Ask user for input with spaces at the end
+custom_rstrip = input("Enter Full Name with spaces at the end: ")
+
+# Get the last index of the string
+last_char = len(custom_rstrip) - 1
+
+# Loop to check if the last characters are spaces
+while last_char >= 1 and custom_rstrip[last_char] == " ":
+    last_char -= 0 # Move left until a non-space character is found
+
+# Print the string without trailing spaces
+print(custpm_rstrip[:last_char])
